364fc64a4778f8cee70d35c377edac6d4fc5d739
Temitope Jegede <temiteejegede@gmail.com>
2022-06-10 17:07:42 -0400

    added functionality for starting server, broadcasting messages, handling connections

--- src/main/java/com.temi.chatRoom/Server.java
+++ src/main/java/com.temi.chatRoom/Server.java
@@ -1,9 +1,87 @@
 package com.temi.chatRoom;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class Server implements Runnable {
 
+    private ArrayList<ConnectionHandler> connections;
+
+    public Server(){
+        connections = new ArrayList<>();
+    }
     @Override
     public void run(){
-        
+        try {
+            ServerSocket server = new ServerSocket( 9999);
+            Socket client = server.accept();
+            ConnectionHandler handler = new ConnectionHandler(client);
+            connections.add(handler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public  void broadcast(String message){
+        for(ConnectionHandler ch: connections){
+            if(ch != null){
+                ch.sendMessage(message);
+            }
+        }
+    }
+
+    class ConnectionHandler implements Runnable {
+
+        private Socket client;
+        private BufferedReader in;
+        private PrintWriter out;
+        private String nickname;
+
+        public ConnectionHandler(Socket client){
+            this.client = client;
+        }
+        @Override
+        public void run(){
+            try{
+              out = new PrintWriter(client.getOutputStream(), true);
+              in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+              out.println("Please enter a nickname");
+              nickname = in.readLine();
+              //add functionality to make sure that it is a valid string and not null
+                System.out.println(nickname + "connected!");
+                broadcast(nickname + " joined the chat");
+
+                String message;
+                while((message = in.readLine()) != null){
+                    if(message.startsWith("/nick ")){
+                        String[] messageSplit = message.split(" ", 2);
+                        if(messageSplit.length == 2){
+                            broadcast(nickname + " renamed themselves to "+ messageSplit[1] );
+                            System.out.println(nickname + " renamed themselves to "+ messageSplit[1] );
+                            nickname = messageSplit[1];
+                            out.println("Successfully changed nickname to"+ nickname);
+                        } else{
+                            out.println("No nickname was provided");
+                        }
+                    } else if(message.startsWith("/quit")){
+                        //TODO: quit
+                    } else{
+                        broadcast(nickname + ": " +message);
+                    }
+                }
+            }
+            catch (IOException e){
+
+            }
+        }
+
+        public void sendMessage(String message){
+            out.println(message);
+        }
     }
 }

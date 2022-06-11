4b2f09c67292e095a007ea2ea6a6c258f84c4e6d
Temitope Jegede <temiteejegede@gmail.com>
2022-06-10 22:21:29 -0400

    Added test class and finished up the client

--- src/test/java/com.temi.chatRoom/ClientTest.java
+++ src/test/java/com.temi.chatRoom/ClientTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 
 public class ClientTest {
 
+    /**
+     * This tests if the server starts up properly on the assigned port
+     */
     @Test
     void testIfServerStarts(){
 
